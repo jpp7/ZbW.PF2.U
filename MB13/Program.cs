1f4e855642b378d243e06350a7799f303e9bc014
Larissa Fitze <laerryies@gmail.com>
2024-07-19 19:17:20 +0200

    remove

--- MB13/Program.cs
+++ MB13/Program.cs
@@ -1,9 +1,2 @@
-﻿
-class Program
-{
-    static void Main(string[] args)
-    {
-
-       
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
