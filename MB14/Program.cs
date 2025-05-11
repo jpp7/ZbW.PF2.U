f5aadffe59ed2be403abb055721b9ffbea820036
Jean-Pierre Pokora <jp_pokora@hotmail.com>
2025-05-11 20:57:17 +0200

    unter

--- MB14/Program.cs
+++ MB14/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using MB14.QueueExample;
+
 Console.WriteLine("Hello, World!");
+
+
+var x = new QueueLinkedList();
+
+DateTime dateTime = DateTime.Now;
+string itemText = "hello";
+string itemName = "name";
+
+x.Enqueue(dateTime);
+x.Enqueue(itemText);
+x.Enqueue(itemName);
+
+var item = x.Dequeue();
+item = x.Dequeue();
+
+
+for(int i=0; i < 10; i++) {
+    x.Peek();
+}
+    
+
+item = x.Dequeue();
+
+
