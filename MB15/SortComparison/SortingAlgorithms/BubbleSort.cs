29b2c5655f37ff9e609b6d33694659fad9bc5e00
Jean-Pierre Pokora <jp_pokora@hotmail.com>
2025-05-14 19:28:05 +0200

    add sort

--- MB15/SortComparison/SortingAlgorithms/BubbleSort.cs
+++ MB15/SortComparison/SortingAlgorithms/BubbleSort.cs
@@ -8,28 +8,28 @@ namespace MB15.SortComparison.SortingAlgorithms
 
         public override void Sort(IList<int> arrayToSort)
         {
-            // int n = arrayToSort.Count;
-            // while (n > 1)
-            // {
-            //     int lastSwapIndex = 0;
-            //     for (int j = 1; j < n; j++)
-            //     {
-            //         if (arrayToSort[j - 1] > arrayToSort[j])
-            //         {
-            //             SwapItems(arrayToSort, j - 1, j);
-            //             lastSwapIndex = j;
-            //         }
-            //     }
-            //
-            //     n = lastSwapIndex; // Ab hier ist alles sortiert
-            // }
+            int n = arrayToSort.Count;
+            while (n > 1)
+            {
+                int lastSwapIndex = 0;
+                for (int j = 1; j < n; j++)
+                {
+                    if (arrayToSort[j - 1] > arrayToSort[j])
+                    {
+                        SwapItems(arrayToSort, j - 1, j);
+                        lastSwapIndex = j;
+                    }
+                }
+            
+                n = lastSwapIndex; // Bis hier ist alles sortiert
+            }
         }
 
         private void SwapItems(IList<int> arrayToSort, int index1, int index2)
         {
-            // var temp = arrayToSort[index1];
-            // arrayToSort[index1] = arrayToSort[index2];
-            // arrayToSort[index2] = temp;
+            var temp = arrayToSort[index1];
+            arrayToSort[index1] = arrayToSort[index2];
+            arrayToSort[index2] = temp;
         }
     }
 }
